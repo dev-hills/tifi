ec9e4ef22d36aefb432a5850952ca07bd5a5c71d
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-10-21 16:09:07 +0100

    push

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,12 +1,13 @@
-import hamburgerMenu from "../../public/assets/hamburgerMenu.svg";
-import React from 'react'
+import hamburgerMenu from "/public/assets/hamburgerMenu.svg";
+import React, { useState } from 'react'
 
 export default function Hamburger() {
+  const [open, setOpen] =useState(false)
   return (
     <div className="lg:hidden flex">
-    <div className="p-3">
+    <button onClick={() => setOpen(prev => !prev)} className="p-3">
         <img src={hamburgerMenu} width={24} height={24} alt="menu icon"/>
-    </div>
+    </button>
 
 </div>
   )
