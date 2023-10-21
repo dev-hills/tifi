20034ec04670f3939a71c3cb6d982404de84cedc
Emujede Hilary <104493382+dev-hills@users.noreply.github.com>
2023-10-21 10:35:42 +0100

    Built FAQ section and business page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      "jakarta-sans": ["Plus Jakarta Sans", 'sans-serif'],
-      "noto-sans": ['Noto Sans', 'sans-serif'],
-      "Red-Hat": ['Red Hat Display', 'sans-serif']
+      "jakarta-sans": ["Plus Jakarta Sans", "sans-serif"],
+      "noto-sans": ["Noto Sans", "sans-serif"],
+      "Red-Hat": ["Red Hat Display", "sans-serif"],
     },
     extend: {},
   },
