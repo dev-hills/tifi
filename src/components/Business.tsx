14dc362d2b16e6fa383a440e244172d42928a99e
ukaoha <ukaohachizoba6@gmail.com>
2023-10-21 13:50:25 +0100

    responsive

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Business: React.FC = () => {
   return (
-    <form action="/#" method="post" className="flex flex-col w-[40%] mx-auto mb-28">
-      <label className="text-gray-700 mb-6">Company Name</label>
+    <form action="/#" method="post" className="flex flex-col w-[90%] md:w-[57%] lg:w-[40%] mx-auto mb-28">
+      <label className="text-gray-700 mt-6 mb-2">Company Name</label>
       <input
         type="text"
         name="companyName"
         placeholder="Company Name"
-        className="mb-4 p-3 rounded-md border border-[#D0D5DD] focus:outline-none focus:border-orange-500"
+        className="mb-4 p-3 rounded-md  border border-[#D0D5DD] focus:outline-none focus:border-orange-500"
       />
 
       <label className="text-gray-700 mb-2">Email Address</label>
@@ -39,7 +39,7 @@ const Business: React.FC = () => {
         type="submit"
         className="bg-orange-500 text-white p-3 rounded-md"
       >
-        Submit
+        Send Message
       </button>
     </form>
   );
